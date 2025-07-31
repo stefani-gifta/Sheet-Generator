abc8fffd4b2d9f2fe9630feedec017898e55c612
stefani-gifta <stefanigifta@gmail.com>
2025-07-31 14:15:58 +0700

    Add arrow & hover

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Welcome to<br/>Monitoring & References Template Generator</h1>
-      <p>Cukup masukkan nama acara, daftar divisi, dan pilih template. Sistem akan langsung menyiapkan dokumen siap pakai untukmu!</p>
-      <button>Start Now</button>
+      <h1>Welcome to<br />Monitoring & References Template Generator</h1>
+      <p class="page-description">Cukup masukkan nama acara, daftar divisi, dan pilih template. Sistem akan langsung menyiapkan dokumen siap pakai untukmu!</p>
+      <button>Start Now<span class="arrow">→</span></button>
     </>
   );
 }
