e998d4227aa94c56c78a62042271e7c884facfc3
stefani-gifta <stefanigifta@gmail.com>
2025-07-31 14:23:58 +0700

    Change landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <section id="home">
       <h1>Welcome to<br />Monitoring & References Template Generator</h1>
       <p class="page-description">Cukup masukkan nama acara, daftar divisi, dan pilih template. Sistem akan langsung menyiapkan dokumen siap pakai untukmu!</p>
       <button>Start Now<span class="arrow">→</span></button>
-    </>
+    </section>
   );
 }
 
