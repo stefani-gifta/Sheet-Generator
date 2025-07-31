6f3b5c5d07a5ba26d0c17daf9b1a7fa9a57fd0a3
stefani-gifta <stefanigifta@gmail.com>
2025-07-31 18:33:07 +0700

    Add button hidden class

--- client/src/BackButton.jsx
+++ client/src/BackButton.jsx
@@ -1,3 +1,3 @@
-export default function BackButton({ insertText }) {
-    return <button class="start-button"><span class="left-arrow">←</span>{ insertText }</button>
+export default function BackButton({ insertText, onClick, className }) {
+    return <button onClick={onClick} className={`start-button ${className}`}><span class="left-arrow">←</span>{ insertText }</button>
 }
--- client/src/FormTemplate.jsx
+++ client/src/FormTemplate.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+
 import BackButton from "./BackButton";
 import NextButton from "./NextButton";
 
@@ -5,6 +7,31 @@ import Page1 from "./Page1";
 import Page2 from "./Page2";
 
 export default function FormTemplate() {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 1:
+                return <Page1 />;
+            case 2:
+                return <Page2 />;
+            default:
+                return <Page1 />;
+        }
+    };
+
+    const handleNext = () => {
+        if (currentPage < 4) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const handleBack = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
     return (
         <section id="form">
             <button class="form-back-button">
@@ -20,24 +47,15 @@ export default function FormTemplate() {
             </div>
 
             <div class="form-contents">
-                <Page1 />
-                <Page2 />
-                <Page3 />
-                <Page4 />
+                {renderPage()}
             </div>
 
             <div class="form-buttons">
-                <BackButton insertText={"Kembali"} />
-                <NextButton insertText={"Lanjutkan"} />
+                <BackButton insertText={"Kembali"} onClick={handleBack}
+                    className={currentPage === 1 ? "hidden-button" : ""} />
+                <NextButton insertText={"Lanjutkan"} onClick={handleNext}
+                    className={currentPage === 4 ? "hidden-button" : ""} />
             </div>
         </section>
     );
-}
-
-function Page3() {
-
-}
-
-function Page4() {
-
 }
