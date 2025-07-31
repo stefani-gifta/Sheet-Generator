7e6bf7a08f0eb5ae0ef5e2db59708dca2adf72f0
stefani-gifta <stefanigifta@gmail.com>
2025-07-31 18:05:49 +0700

    Finish Page2

--- client/src/Page2.jsx
+++ client/src/Page2.jsx
@@ -1,3 +1,5 @@
+import logoBSLC from '../assets/BSLC-logo.png';
+
 export default function Page2() {
     const templates = [
         {}
@@ -7,28 +9,22 @@ export default function Page2() {
         <div>
             <h2>Pilihlah satu template yang ingin digunakan</h2>
             <div class="template-container">
-                <label class="template-card">
-                    <input type="radio" name="template" />
-                    <div class="card-content">
-                        <img src="../assets/BSLC-logo.png" alt="BSLC Logo" class="logo" />
-                        <div class="template-name">BSLC Event A 2024</div>
-                    </div>
-                </label>
-                <label class="template-card">
-                    <input type="radio" name="template" />
-                    <div class="card-content">
-                        <img src="../assets/BSLC-logo.png" alt="BSLC Logo" class="logo" />
-                        <div class="template-name">BSLC Event A 2024</div>
-                    </div>
-                </label>
-                <label class="template-card">
-                    <input type="radio" name="template" />
-                    <div class="card-content">
-                        <img src="../assets/BSLC-logo.png" alt="BSLC Logo" class="logo" />
-                        <div class="template-name">BSLC Event A 2024</div>
-                    </div>
-                </label>
+                <TemplateCard eventName={"Event A 2024"} />
+                <TemplateCard eventName={"Event A 2023"} />
+                <TemplateCard eventName={"Event A 2025"} />
             </div>
         </div>
+    );
+}
+
+function TemplateCard({ eventName }) {
+    return (
+        <label class="template-card">
+            <input type="radio" name="template" />
+            <div class="card-content">
+                <img src={logoBSLC} alt="BSLC Logo" class="logo" />
+                <div class="template-name">{ eventName }</div>
+            </div>
+        </label>
     );
 }
