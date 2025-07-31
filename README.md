2d609969034d878e49d2228ea5682facaa6f81cc
stefani-gifta <stefanigifta@gmail.com>
2025-07-31 13:45:34 +0700

    Add watch

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Monitoring (&amp; References) Sheet Generator
 
 <!-- npm install, npm run build, npm start -->
+<!-- https://stackoverflow.com/questions/60424844/do-i-need-to-run-npm-run-build-every-time-i-made-changes -->
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Welcome to Monitoring & References Template Generator</h1>
-      <span>Cukup masukkan nama acara, daftar divisi, dan pilih template — sistem akan langsung menyiapkan dokumen siap pakai untukmu!</span>
+      <h1>Welcome to<br/>Monitoring & References Template Generator</h1>
+      <div>Cukup masukkan nama acara, daftar divisi, dan pilih template. Sistem akan langsung menyiapkan dokumen siap pakai untukmu!</div>
       <button>Start Now</button>
     </>
   );
