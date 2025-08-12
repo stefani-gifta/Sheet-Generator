d44b737c798bbb8119f80c47edd6ca81b5629b87
yechika <mantuldanil@gmail.com>
2025-08-12 14:57:03 +0700

    BE koreksi

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const ExcelJS = require('exceljs');
@@ -5,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
